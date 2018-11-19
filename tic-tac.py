689f5d61f9b01e75238c3bbac003bde7ec7d5025
Harmony2 <flyingflows@gmail.com>
2018-11-18 22:30:15 -0500

    changing library from unicurses to curses

--- tic-tac.py
+++ tic-tac.py
@@ -1,68 +1,33 @@
 #!/usr/bin/python3
-
-from unicurses import *
-
-def welcome(stdscr):
-	stdscr.addstr(1,1,"Hello, bro")
-	stdscr.addstr(2,1,"this would be tic tac toe")
-	stdscr.addstr(5,5,"Press any key to continue.")
-	stdscr.getch()
-
-def clear(stdscr):
-	stdscr.clear()
-	stdscr.border()
-	stdscr.refresh()
-
-def table(stdscr, pos):
-	centx = getmaxyx(stdscr)[1]//2
-	centy = getmaxyx(stdscr)[0]//2
+from curses import *
 
 
-	stdscr.addstr(centy-2,centx-2, "|")
-	stdscr.addstr(centy-2,centx+1, "|")
-	stdscr.addstr(centy-1,centx-4, "--------")
-	stdscr.addstr(centy,centx-2, "|")
-	stdscr.addstr(centy,centx+1, "|")
-	stdscr.addstr(centy+1,centx-4, "--------")
-	stdscr.addstr(centy+2,centx-2, "|")
-	stdscr.addstr(centy+2,centx+1, "|")
+def mov(window,atty,attx):
+	if window.getkey() == "a":
+		attx = attx-1
+	elif window.getkey() == "d":
+		attx = attx+1
 
-	stdscr.move(centy,centx)
-
-	return pos
-
-def mov(y,x):
-	input = getkey()
-
-	if input == "w":
-		y = y-1
-	elif input == "s":
-		y = y+1
-	elif input == "d":
-		x = x+1
-	elif input == "a":
-		x = x-1
-	elif input == "q":
-		quit()
-	move(y,x)
-	mov(y,x)
+	window.move(atty,attx)
+	#window.redrawwin()
+	window.refresh()
+	mov(window,atty,attx)
 
 def main():
-	stdscr = initscr()
-	centx = getmaxyx(stdscr)[1]//2
-	centy = getmaxyx(stdscr)[0]//2
+	window = initscr()
+	window.addstr(1,1,"teste")
 
-	stdscr.clear()
-	stdscr.border()
+	centy = window.getmaxyx()[0]//2
+	centx = window.getmaxyx()[1]//2
 
-	welcome(stdscr)
-	clear(stdscr)
-	stdscr.refresh()
+	window.addstr(2,1,str(centx))
+	window.addstr(3,1,str(centy))
+	window.getch()
 
-	pos = [['O','X','O'],[' ',' ',' '],[' ',' ',' ']]
-	table(stdscr,pos)
+	window.clear()
+	window.refresh()
+	window.border()
 
-	while True:
-		mov(centy,centx)
+	mov(window,centy,centx)
 
 main()
